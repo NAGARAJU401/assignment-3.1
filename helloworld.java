2e389c11d5d57a6a2826bd1c55ccd31ebbe2ab0e
NAGARAJU401 <31498449+NAGARAJU401@users.noreply.github.com>
2017-09-12 11:51:01 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,21 @@
+package assignment3;
+
+public class helloworld {
+//public is a Java keyword which declares a member's access as public.
+//class can be defined as a template that describes the state that the object of its type support.
+	public static void main(String[] args) {
+		//Public is a keyword that is used as an access modifier for methods and variables.
+		//static used to prepare a field,method or inner classes as a class field.
+//void: it is void if the method does not return a value.
+//main:it is a function name When a program starts running, it has to start execution from somewhere.
+//String[] args: it is  defining a String array to pass arguments at command line. args is the variable name of the String array.
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		//system : system is a class in java language.lang package
+		//out : out is the static member of system class.It's type PrintStream
+		//println: which is used to print the output.
+
+		
+	}
+
+}
